a3389f5d21889e56821b3704948012349effd004
Lariel <lariel.negreiros@pucrs.br>
2021-08-18 18:15:04 -0300

    ajustes Api model annotations

--- src/main/java/com/handler/api/model/PersonDto.java
+++ src/main/java/com/handler/api/model/PersonDto.java
@@ -7,6 +7,7 @@ import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,18 +18,38 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PersonDto {
+	@ApiModelProperty(hidden = true)
 	private String id;
+
+	@ApiModelProperty(required = true, position = 0)
 	@NotNull(message = "Primeiro nome é um campo obrigatório")
 	private String firstName;
+
+	@ApiModelProperty(required = true, position = 1)
 	@NotNull(message = "Sobrenome é um campo obrigatório")
 	private String lastName;
+	
+	@ApiModelProperty(example = "dd/MM/yyyy", required = true, position = 2)
 	@NotNull(message = "Data de nascimento é obrigatória")
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
 	private LocalDate birthDay;
+
+
+	@ApiModelProperty(hidden = true)
 	private String age;
-	private String gender;
+
+	@ApiModelProperty(required = false, position = 3)
 	private String cpf;
+
+	@ApiModelProperty(required = false, position = 4)
+	private String gender;
+
+	@ApiModelProperty(hidden = true)
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm")
 	private LocalDateTime created;
+
+	@ApiModelProperty(hidden = true)
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm")
 	private LocalDateTime lastUpdate;	
 
 	public static PersonDto buildFromDocument(Person person) {
